e027f733f2c59ce9d3a09e84c7aecafc89aa668b
piwasiewicz <piwasiewicz@gmail.com>
2019-04-15 00:34:37 +0200

    default user guest

--- base-notebook/test/test_container_options.py
+++ base-notebook/test/test_container_options.py
@@ -44,7 +44,7 @@ def test_uid_change(container):
     )
     # usermod is slow so give it some time
     c.wait(timeout=120)
-    assert 'uid=1010(jovyan)' in c.logs(stdout=True).decode('utf-8')
+    assert 'uid=1010(guest)' in c.logs(stdout=True).decode('utf-8')
 
 
 def test_gid_change(container):
@@ -57,8 +57,8 @@ def test_gid_change(container):
     )
     c.wait(timeout=10)
     logs = c.logs(stdout=True).decode('utf-8')
-    assert 'gid=110(jovyan)' in logs
-    assert 'groups=110(jovyan),100(users)' in logs
+    assert 'gid=110(guest)' in logs
+    assert 'groups=110(guest),100(users)' in logs
 
 
 def test_chown_extra(container):
